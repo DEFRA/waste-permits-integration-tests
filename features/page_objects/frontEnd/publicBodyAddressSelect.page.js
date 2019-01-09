996c16fea3060d121199b088ef254af2f2e946fd
ben <ben@surgison.net>
2019-01-09 17:15:47 +0000

    Fix charity public body test

--- features/page_objects/frontEnd/publicBodyAddressSelect.page.js
+++ features/page_objects/frontEnd/publicBodyAddressSelect.page.js
@@ -2,6 +2,7 @@ const AddressSelectPageObject = require('./base/AddressSelectPageObject').Addres
 
 class PublicBodyAddressSelectPage extends AddressSelectPageObject {
   get title () { return 'What is the main address for the local authority or public body?' }
+  get charityTitle () { return 'What is the main address for the public body?' }
 }
 
 module.exports = PublicBodyAddressSelectPage
