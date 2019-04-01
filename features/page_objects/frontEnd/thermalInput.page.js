aac63efabcf7ec131628e6469e28cda87a127eea
ben <ben@surgison.net>
2019-04-01 08:07:07 +0100

    Fix tests for page-heading change to thermal input page.

--- features/page_objects/frontEnd/thermalInput.page.js
+++ features/page_objects/frontEnd/thermalInput.page.js
@@ -1,7 +1,7 @@
 const FrontEndPageObject = require('./base/FrontEndPageObject').FrontEndPageObject
 
 class ThermalInputPage extends FrontEndPageObject {
-  get title () { return 'About your generators' }
+  get title () { return 'Does any single generator unit have a rated thermal input between 20MW and 50MW?' }
   get 'thermal-rating-not-20-to-50' () { return { css: '#thermal-rating-not-20-to-50' } }
   get 'thermal-rating-20-to-50' () { return { css: '#thermal-rating-20-to-50' } }
   get 'engine-type-boiler-etc' () { return { css: '#engine-type-boiler-etc' } }
