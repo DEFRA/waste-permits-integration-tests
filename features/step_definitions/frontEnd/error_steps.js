b80e3dd496eccc8050d55bf881506ae5aaa0c150
ben <ben@surgison.net>
2019-05-20 14:19:39 +0100

    Upgrade cucumber to version 5

--- features/step_definitions/frontEnd/error_steps.js
+++ features/step_definitions/frontEnd/error_steps.js
@@ -1,20 +1,18 @@
-var { defineSupportCode } = require('cucumber')
+const { When } = require('cucumber')
 // var path = require('path')
 // const tasks = require('../page_objects/helpers/tasks')
 // const {email, contact, individual, limitedCompany, limitedLiabilityPartnership, soleTrader, site, invoice, confidentialityNeeds} = require('../support/testData')
 
-defineSupportCode(function ({ Given, When }) {
-  When(/^I should see an error "(.*)"$/, async function (message) {
-    const { permitHolderSelectPage } = this.pages.frontEnd
-    return permitHolderSelectPage.checkError(message)
-  })
+When(/^I should see an error "(.*)"$/, async function (message) {
+  const { permitHolderSelectPage } = this.pages.frontEnd
+  return permitHolderSelectPage.checkError(message)
+})
 
-  // I enter a contact with <Field> set to <Value>
-  When(/^I enter a contact with (.*) set to "(.*)"$/, async function (field, value) {
-    const { taskListPage, contactDetailsPage } = this.pages.frontEnd
-    await taskListPage.selectTask(taskListPage.contactDetailsLink)
-    const { contact } = require('../../support/testData')
-    contact[field] = value
-    return contactDetailsPage.completePage(contact)
-  })
+// I enter a contact with <Field> set to <Value>
+When(/^I enter a contact with (.*) set to "(.*)"$/, async function (field, value) {
+  const { taskListPage, contactDetailsPage } = this.pages.frontEnd
+  await taskListPage.selectTask(taskListPage.contactDetailsLink)
+  const { contact } = require('../../support/testData')
+  contact[field] = value
+  return contactDetailsPage.completePage(contact)
 })
--- features/support/hooks.js
+++ features/support/hooks.js
@@ -5,42 +5,40 @@ const { 'world-parameters': { platform }, appUrl, appUrlCRM } = require('../../c
 let frontEndVersion
 let backEndVersion
 
-var { registerHandler } = require('cucumber')
+const { After, AfterAll, BeforeAll, Status } = require('cucumber')
 
-registerHandler('BeforeFeatures', ([feature]) => {
-  const options = {
-    theme: 'bootstrap',
-    jsonDir: 'features/reports',
-    output: 'features/reports/cucumber_report.html',
-    reportSuiteAsScenarios: true,
-    launchReport: true,
-    ignoreBadJsonFile: true,
-    metadata: {
-      'App Version': 'Waste Permits',
-      'FrontEnd Test Environment': appUrl,
-      'BackEnd Test Environment': appUrlCRM,
-      'FrontEnd Version': frontEndVersion,
-      'BackEnd Version': backEndVersion,
-      Browser: platform,
-      // Platform: "Windows 10",
-      Parallel: 'Scenarios',
-      Executed: 'local'
-    }
+const options = {
+  theme: 'bootstrap',
+  jsonDir: 'features/reports',
+  output: 'features/reports/cucumber_report.html',
+  reportSuiteAsScenarios: true,
+  launchReport: true,
+  ignoreBadJsonFile: true,
+  metadata: {
+    'App Version': 'Waste Permits',
+    'FrontEnd Test Environment': appUrl,
+    'BackEnd Test Environment': appUrlCRM,
+    'FrontEnd Version': frontEndVersion,
+    'BackEnd Version': backEndVersion,
+    Browser: platform,
+    // Platform: "Windows 10",
+    Parallel: 'Scenarios',
+    Executed: 'local'
   }
+}
+
+BeforeAll(function () {
   // Make sure the reports directory exists
   mkdirp(options.jsonDir)
-  if (feature) {
-    feature.options = options
-  }
 })
 
-registerHandler('AfterFeatures', ([{ options }]) => {
+AfterAll(function () {
   const reporter = require('cucumber-html-reporter')
   reporter.generate(options)
 })
 
-registerHandler('ScenarioResult', async function (scenario) {
-  if (scenario.status === 'failed') {
+After(async function (testCase) {
+  if (testCase.result.status === Status.FAILED) {
     const filename = await driver.browser.getCurrentUrl()
     driver.takeScreenshotsAfterFailure(filename)
     return driver.quitBrowser()
